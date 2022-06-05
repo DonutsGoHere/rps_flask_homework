bc89141543095261c946dd9fa879707402b3b2ef
Calum Cook <calumcook@Calums-Air.broadband>
2022-06-05 02:27:41 +0100

    2nd commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -13,53 +13,53 @@ def index():
 #    return render_template('game_result')
 
 
-@app.route('/rock')
-def rock():
-   return render_template('rock.html')
+# @app.route('/rock')
+# def rock():
+#    return render_template('rock.html')
 
-@app.route('/paper')
-def paper():
-   return render_template('paper.html')
+# @app.route('/paper')
+# def paper():
+#    return render_template('paper.html')
 
-@app.route('/scissors')
-def scissors():
-   return render_template('scissors.html')
+# @app.route('/scissors')
+# def scissors():
+#    return render_template('scissors.html')
 
 @app.route('/rock/rock')
 def rock_rock():
-   return render_template('draw.html')
+   return render_template('rock_draw.html')
 
 @app.route('/rock/paper')
 def rock_paper():
-   return render_template('player_2_wins.html')
+   return render_template('rp_player_2_wins.html')
 
 @app.route('/rock/scissors')
 def rock_scissors():
-   return render_template('player_1_wins.html')
+   return render_template('rs_player_1_wins.html')
 
 @app.route('/paper/rock')
 def paper_rock():
-   return render_template('player_1_wins.html')
+   return render_template('pr_player_1_wins.html')
 
 @app.route('/paper/paper')
 def paper_paper():
-   return render_template('draw.html')
+   return render_template('paper_draw.html')
 
 @app.route('/paper/scissors')
 def paper_scissors():
-   return render_template('player_2_wins.html')
+   return render_template('ps_player_2_wins.html')
 
 @app.route('/scissors/rock')
 def scissors_rock():
-   return render_template('player_2_wins.html')
+   return render_template('sr_player_2_wins.html')
 
 @app.route('/scissors/paper')
 def scissors_paper():
-   return render_template('player_1_wins.html')
+   return render_template('sp_player_1_wins.html')
 
 @app.route('/scissors/scissors')
 def scissors_scissors():
-   return render_template('draw.html')
+   return render_template('scissors_draw.html')
 
 
 
--- models/player.py
+++ models/player.py
@@ -1,6 +1,7 @@
 class Player:
   def __int__(self, name, choice):
     self.name = name 
-    self.choice = choice
+    self.choice = []
+    # self.choice = [choice]
 
 
